b5e4fa62c493d9ebf94e7be2ee2f425a3118d415
arthurchan1111 <arthurchan1111@gmail.com>
2018-02-11 07:07:31 -0500

    Fix database bugs and add additional columns

--- database_setup.py
+++ database_setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
 import sys
-
+import psycopg2
 from sqlalchemy import Column, ForeignKey, Integer, String, Text, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -9,44 +9,79 @@ from sqlalchemy import create_engine
 Base= declarative_base()
 
 class Contacts(Base):
-    _tablename_ = 'contacts'
+    __tablename__ = 'contacts'
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
     first_name = Column(String(80), nullable= False)
     last_name = Column(String(80), nullable=False)
 
+    @property
+    def serialize(self):
+        return{
+            "first_name" : self.first_name,
+            "last_name" : self.last_name
 
-class Address(Base):
-    _tablename_= 'address'
-    id= Column(Integer, primary_key=True, autoincrement=True, unique=True)
-    address = Column(String(80), nullable=False)
-    contact_id = Column(Integer, ForeignKey('contact.id'))
-    contact = relationship(Contacts, cascade="delete")
+        }
 
 class Policy(Base):
-    _tablename_='policy'
+    __tablename__='policy'
     id  = Column(Integer, primary_key=True, autoincrement=True, unique=True)
-    number = Column(Integer, nullable=False)
+    number = Column(Integer, nullable=False, unique=True)
     effective_date= Column(Date, nullable=False)
     created = Column(Date, nullable=False)
     updated = Column (Date, nullable=False)
-    contact_id = Column(Integer, ForeignKey('contact.id'))
+    contact_id = Column(Integer, ForeignKey('contacts.id'))
     contact = relationship(Contacts, cascade="delete")
 
-class PolicyType(Base):
-    _tablename_ = 'policytype'
-    id  = Column(Integer, primary_key=True, autoincrement=True, unique=True)
-    policy_name = Column(String(80), nullable = False)
+    @property
+    def serialize(self):
+        return{
+            "policy_number" : self.number,
+            "effective_date" : self.effective_date,
+            "created": self.created,
+            "updated": self.updated
+
+        }
+
+class Address(Base):
+    __tablename__= 'address'
+    id= Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    address = Column(String(80), nullable=False)
+    contact_id = Column(Integer, ForeignKey('contacts.id'))
+    contact = relationship(Contacts, cascade="delete")
     policy_id = Column(Integer, ForeignKey('policy.id'))
     policy = relationship(Policy, cascade="delete")
 
+    @property
+    def serialize(self):
+        return{
+            "address" : self.address
+
+        }
+
+class PolicyType(Base):
+    __tablename__ = 'policytype'
+    id  = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    policy_type = Column(String(80), nullable = False)
+    policy_id = Column(Integer, ForeignKey('policy.id'))
+    policy = relationship(Policy, cascade="delete")
+
+    @property
+    def serialize(self):
+        return{
+            "policy_type" : self.policy_type,
+
+
+        }
+
 class Letter(Base):
-    _tablename_= "letter"
+    __tablename__= "letter"
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
     name = Column(String(80), nullable= False)
     description = Column(String(200))
+    title = Column(String(80))
     text = Column(Text, nullable=False)
-    policy_type = Column(Integer, ForeignKey('policytype.id'))
-    policy = relationship(PolicyType, cascade="delete")
+    policy_type = Column(String(80))
+
 
 
 engine = create_engine('postgresql:///farmerfire')
