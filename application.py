9c00849a90a15c986c30bacbf06b3d587926e302
arthurchan1111 <arthurchan1111@gmail.com>
2018-02-20 10:22:30 -0500

    Basic backend functionality complete

--- application.py
+++ application.py
@@ -2,41 +2,78 @@
 from database_setup import Base, Contacts, Address, Policy, PolicyType, Letter
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, make_response, g
+from flask import session as document_session
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from datetime import date
-import json
+import pdfkit
+
+
 
 app = Flask(__name__)
 admin = Admin(app)
+
 
 engine = create_engine('postgresql:///farmerfire')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-admin.add_view(ModelView(Contacts,session))
-admin.add_view(ModelView(Address,session))
-admin.add_view(ModelView(Policy,session))
-admin.add_view(ModelView(PolicyType,session))
-admin.add_view(ModelView(Letter,session))
+admin.add_view(ModelView(Contacts, session))
+admin.add_view(ModelView(Address, session))
+admin.add_view(ModelView(Policy, session))
+admin.add_view(ModelView(PolicyType, session))
+admin.add_view(ModelView(Letter, session))
 
 session.rollback()
 
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    letter_categories = session.query(Letter.policy_type).distinct().all()
-    print letter_categories[0].policy_type
-    return render_template('index.html', letter_categories= letter_categories)
 
-@app.route('/letters/<int:id>', methods=['GET'])
-def letter(id):
+    if request.method == 'POST':
+        data= request.get_json()
+        id= data["letter_id"]
+        resultlist = data["data"]
+        url_base= "http://localhost:5000/letters/"+str(id)+"?pnum="
+        urls = []
+
+        if str(resultlist[0]).isdigit():
+            for x in range(len(resultlist)):
+                urls.append(url_base + str(resultlist[x]))
+
+            return jsonify(Url = urls)
+
+        for x in range(len(resultlist)):
+            address = session.query(Address).filter(Address.address == resultlist[x]).first()
+            policy = session.query(Policy).filter(Policy.id == address.policy_id).first()
+            urls.append(url_base + str(policy.number))
+
+        return jsonify(Url = urls)
+
+
+    else:
+        general = session.query(Letter).filter(Letter.policy_type == "General").all()
+        commercial = session.query(Letter).filter(Letter.policy_type == "Commercial").all()
+        dwellers = session.query(Letter).filter(Letter.policy_type == "Dwellers Fire").all()
+        home = session.query(Letter).filter(Letter.policy_type == "Homeowners").all()
+        return render_template('index.html', general=general, commercial= commercial, dwellers=dwellers, home=home)
+
+
+@app.route('/letters/<int:id>', methods=['GET', 'POST'])
+def letter(id, **kwargs):
+    pnum = request.args.get('pnum')
+    document_session['pnum'] = pnum
+    policy = session.query(Policy).filter(Policy.number == pnum).first()
+    contact = session.query(Contacts).filter(Contacts.id == policy.contact_id).first()
+    address = session.query(Address).filter(Address.policy_id == policy.id).first()
+    address_heading = address.address.split(",", 1)
+    policy_type = session.query(PolicyType).filter(PolicyType.policy_id == policy.id).first()
     today = (str(date.today().strftime('%m/%d/%Y')))
     letter = session.query(Letter).filter(Letter.id == id).one()
-    return render_template('letter_template.html', date=today, letter=letter)
+    return render_template('letter_template.html', id=id, date=today, letter=letter, policy=policy, contact = contact, address = address, policy_type = policy_type, address_heading = address_heading)
 
 @app.route('/JSON/policy/<int:policy>')
 def getPolicy(policy):
@@ -44,66 +81,101 @@ def getPolicy(policy):
         policy = session.query(Policy).filter(Policy.number == policy).first()
 
         if policy:
-            contact = session.query(Contacts).filter(Contacts.id == policy.contact_id).first()
-            address = session.query(Address).filter(Address.policy_id == policy.id).first()
-            policy_type = session.query(PolicyType).filter(PolicyType.policy_id == policy.id).first()
-            return jsonify(Policy=[contact.serialize,address.serialize,policy.serialize,policy_type.serialize])
+            contact = session.query(Contacts).filter(
+                Contacts.id == policy.contact_id).first()
+            address = session.query(Address).filter(
+                Address.policy_id == policy.id).first()
+            policy_type = session.query(PolicyType).filter(
+                PolicyType.policy_id == policy.id).first()
+            return jsonify(Policy=[contact.serialize, address.serialize, policy.serialize, policy_type.serialize, "pn"])
 
-        return json.dumps({"error": "No match found"})
+        return make_response(jsonify({'error': 'Not found'}), 404)
 
     except:
-        return json.dumps({"error": "No match found"})
+        return make_response(jsonify({'error': 'Not found'}), 404)
 
-@app.route('/JSON/contacts/<string:name>')
-def getContacts(name):
-    full_name = name.split("_")
+
+@app.route('/JSON/contacts')
+def getContacts():
     try:
-        contacts = session.query(Contacts).filter(Contacts.first_name == full_name[0]).filter(Contacts.last_name == full_name[1]).limit(100).all()
-        if len(contacts) > 0:
-            for x in range(len(contacts)):
-                address = session.query(Address).filter(Address.contact_id == contacts[x].id).limit(10).all()
-                track.append(contacts[x])
+        first_name = request.args.get("fname")
+        last_name = request.args.get("lname")
+        if last_name is None:
+            contacts = session.query(Contacts).filter(Contacts.first_name == first_name).limit(100).all()
+            return jsonify(Policy= iterContacts(contacts))
 
-            return jsonify(Stuff= track)
-                 
-        return json.dumps({"error": "No match found"})
+        else:
+            contacts = session.query(Contacts).filter(Contacts.first_name == first_name).filter(Contacts.last_name == last_name).limit(100).all()
+            return jsonify(Policy= iterContacts(contacts))
+
+        return make_response(jsonify({'error': 'Not found'}), 404)
 
     except:
-        return json.dumps({"error": "No match found"})
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 @app.route('/JSON/address/<string:address>')
 def getAddress(address):
     try:
-        address = session.query(Address).filter(Address.address == address).first()
+        address = session.query(Address).filter(
+            Address.address == address).first()
         if address:
-            contact = session.query(Contacts).filter(Contacts.id == address.contact_id).first()
-            policy = session.query(Policy).filter(Policy.id == address.policy_id).first()
-            policy_type = session.query(PolicyType).filter(PolicyType.policy_id == address.policy_id).first()
-            return jsonify(Policy=[contact.serialize,address.serialize,policy.serialize,policy_type.serialize])
-        return json.dumps({"error": "No match found"})
+            contact = session.query(Contacts).filter(
+                Contacts.id == address.contact_id).first()
+            policy = session.query(Policy).filter(
+                Policy.id == address.policy_id).first()
+            policy_type = session.query(PolicyType).filter(
+                PolicyType.policy_id == address.policy_id).first()
+            return jsonify(Policy=[contact.serialize, address.serialize, policy.serialize, policy_type.serialize, "add"])
+        return make_response(jsonify({'error': 'Not found'}), 404)
 
     except:
-        return json.dumps({"error": "No match found"})
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 @app.route('/JSON/policytype/<string:ptype>')
 def getPolicyType(ptype):
     try:
-        policytype= session.query(PolicyType).filter(PolicyType.policy_type == ptype.title()).limit(100).all()
-        if len(policytype) == 0:
-            return json.dumps({"error": "No match found"})
 
-        print policytype[0].policy_id
-        return "hey"
+        policytype = session.query(PolicyType).filter(PolicyType.policy_type == ptype).limit(100).all()
+        if len(policytype) > 0:
+            policyTypes = []
+            for x in range(len(policytype)):
+                policyinfo = session.query(Policy).filter(Policy.id == policytype[x].policy_id).first()
+                policy = {"policy": policytype[x].policy_type,
+                          "policy_number": policyinfo.number}
+
+                policyTypes.append(policy)
+
+            policyTypes.append("pt")
+
+            return jsonify(Policy=policyTypes)
+
+        return make_response(jsonify({'error': 'Not found'}), 404)
 
     except:
-        return json.dumps({"error": "No match found"})
+        return make_response(jsonify({'error': 'Not found'}), 404)
 
-@app.route('/JSON/advanced/<data>')
-def advancedSearch():
-    pass
+
+def iterContacts(contactList):
+    result = []
+    if len(contactList) > 0:
+        for x in range(len(contactList)):
+            address = session.query(Address).filter(Address.contact_id == contactList[x].id).all()
+            print address
+            addresslist = [a.address for a in address]
+            print addresslist
+            contact = {"id": contactList[x].id,"first_name": contactList[x].first_name,
+                        "last_name": contactList[x].last_name, "addresses": addresslist}
+            result.append(contact)
+
+        result.append("ct")
+    return result
+
 
 
 if __name__ == '__main__':
-    app.debug= True
+    app.debug = True,
+    app.config['SECRET_KEY'] = 'super_secret_key'
     app.secret_key = 'super_secret_key'
     app.run(host='0.0.0.0', port=5000)
